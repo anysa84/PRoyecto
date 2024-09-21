8b1809ede2acd10ee6402cfd6c6b982352288301
anysa84 <anahi.vsa@gmail.com>
2024-09-21 18:50:08 -0300

    Version 3

--- tours.js
+++ tours.js
@@ -2,3 +2,11 @@ const nombre = prompt("¿Cuál es tu nombre?");
 const edad = prompt("¿Cuántos años tienes?");
 const mensaje = `Hola, ${nombre}. Tienes ${edad} años. ¡Bienvenido!`;
 console.log(mensaje);
+let HOLA = nombre
+nombre === "¿Cuál es tu nombre? "
+if (nombre de Usuatio ) {nombre !== nombre
+    console.log ( "completar sus datos")
+} else{
+    "¡Hola, " + nombre + "! ¡Bienvenido!";
+}
+
