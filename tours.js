570a2effdb259262d6a0f438862fb48581731597
anysa84 <anahi.vsa@gmail.com>
2024-09-21 17:37:23 -0300

    Version 2

---
+++ tours.js
@@ -0,0 +1,4 @@
+const nombre = prompt("¿Cuál es tu nombre?");
+const edad = prompt("¿Cuántos años tienes?");
+const mensaje = `Hola, ${nombre}. Tienes ${edad} años. ¡Bienvenido!`;
+console.log(mensaje);
